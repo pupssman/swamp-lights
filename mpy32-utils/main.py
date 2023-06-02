05e38cccdd81cb220a8dd442ee6b8de482f0010c
Ivan Kalinin <pupssman@gmail.com>
2023-06-02 22:30:47 +0300

    micropython is a go

--- mpy32-utils/main.py
+++ mpy32-utils/main.py
@@ -3,7 +3,7 @@ import time
 led = machine.Pin(2, machine.Pin.OUT)
 
 for i in range(10):
-	led.value(1)
-	time.sleep(1)
-	led.value(0)
-	time.sleep(1)
+    led.value(1)
+    time.sleep(1)
+    led.value(0)
+    time.sleep(1)
--- mpy32-utils/test_ws2812.py
+++ mpy32-utils/test_ws2812.py
@@ -13,18 +13,21 @@ led = machine.Pin(2, machine.Pin.OUT)
 
 
 def light_full(color):
-    np.fill(color)
+    for n in range(num_pixels):
+        np[n] = color
+    # np.fill(color)
     np.write()
 
 
-COLORS = [(0, 0, 0)] + \
-    [(n * 4, 0, 0) for n in range(32)] + \
-    [(0, n * 4, 0) for n in range(32)] + \
-    [(0, 0, n * 4) for n in range(32)] + \
-    [(0, 0, 0)] * 3
+G = [(n * 4, 0, 0) for n in range(32)]
+B = [(0, n * 4, 0) for n in range(32)]
+R = [(0, 0, n * 4) for n in range(32)]
+K = [(0, 0, 0)] * 3
+
+COLORS = K + G + G[::-1] + B + B[::-1] + R + R[::-1] + K
 
 for c in COLORS:
     led.value(1)
     light_full(c)
     led.value(0)
-    time.sleep(.1)
+    time.sleep(.05)
