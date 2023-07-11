4739a8105b2268642dc03f4508c9a58846f448b0
Ivan Kalinin <pupssman@gmail.com>
2023-07-12 00:53:53 +0300

    more worldy stuff

--- hub/world.py
+++ hub/world.py
@@ -16,6 +16,7 @@ class Room:
     WALKER = Room(3, 3 * 60)
     SWAMP = Room(4, None)
     TREE = Room(5, 5 * 60)
+    COLUMN = Room(6, None)
 
 
 class RoomState:
@@ -71,6 +72,7 @@ class World:
         self.room_state = {
             r: RoomState.INITIAL for r in Room
         }
+        self.room_state[Room.ENTRY] = RoomState.PRIMARY  # in reset we wait for new party
 
     def get_device_state(self, room):
         return self.room_state[room]
@@ -95,6 +97,8 @@ class World:
         except Exception as e:
             print('NOT EVENT: %s / %s' % (event_name, e))
             return
+
+        self.change_time = time.time()
 
         if event == Event.RESET:
             return self.reset()
@@ -124,21 +128,86 @@ class World:
             print('CANT HANDLE -- %s in %s' % (event, self.aactive_room))
 
     def enter_dream(self):
-        # FIXME
-        pass
+        self.active_room = Room.ENTRY
+        self.room_events = [Event.START_WALKER]
+        self.room_state = {
+            r: RoomState.INITIAL for r in Room
+        }
 
-    def handle_device_event(self, event):
-        # FIXME
-        pass
-
-    def handle_walker(self, event):
-        pass
-
-    def handle_swamp(self, event):
-        pass
-
-    def handle_tree(self, event):
-        pass
+        self.room_state[Room.ENTRY] = RoomState.HIGH # light is in primary room
+        # FIXME: PLAY_MUSIC
 
     def return_from_dream(self):
-        pass
+        self.active_room = Room.ENTRY
+        self.room_events = []  # nothing can happen
+        self.room_state = {
+            r: RoomState.INITIAL for r in Room
+        }
+
+        self.room_state[Room.ENTRY] = RoomState.HIGH # light is in primary room
+        # FIXME: PLAY_MUSIC
+
+    def handle_device_event(self, event):
+        if event == Event.PLUG_WALL_A:
+            self.wall_a_plugged = True
+            self.check_plugs()
+        elif event == Event.UNPLUG_WALL_A:
+            self.wall_a_plugged = False
+            self.check_plugs()
+        elif event == Event.PLUG_WALL_B:
+            self.wall_b_plugged = True
+            self.check_plugs()
+        elif event == Event.UNPLUG_WALL_B:
+            self.wall_b_plugged = False
+            self.check_plugs()
+        elif event == Event.COLUMN_LOW:
+            self.column_up = False
+            # TODO: disable column
+        elif event == Event.COLUMN_RISE:
+            self.column_up = True
+            # TODO: activate column timer
+
+    def check_plugs(self):
+        if self.wall_a_plugged and self.wall_b_plugged:
+            # all plugged, elevate room state
+            self.room_state[Room.CATACOMBS] = RoomState.HIGH  # FIXME: more modes for room
+            # TODO: play music
+
+    def handle_walker(self, event):
+        self.active_room = Room.WALKER
+        self.room_events = [Event.WALKER_ENRAGE, Event.START_SWAMP]
+        self.room_state = {
+            r: RoomState.INITIAL for r in Room
+        }
+
+        if event == Event.START_WALKER:
+            self.room_state[Room.WALKER] = RoomState.PRIMARY
+        else:
+            self.room_state[Room.WALKER] = RoomState.HIGH
+        # FIXME: play music
+
+    def handle_swamp(self, event):
+        self.active_room = Room.SWAMP
+        self.room_events = [Event.SWAMP_ENRAGE, Event.ENTER_TREE]
+        self.room_state = {
+            r: RoomState.INITIAL for r in Room
+        }
+
+        if event == Event.START_SWAMP:
+            self.room_state[Room.SWAMP] = RoomState.PRIMARY
+        else:
+            self.room_state[Room.SWAMP] = RoomState.HIGH
+        # FIXME: play music
+
+    def handle_tree(self, event):
+        self.active_room = Room.TREE
+        self.room_events = [Event.TREE_ENRAGE, Event.RETURN_FROM_DREAM]
+        self.room_state = {
+            r: RoomState.INITIAL for r in Room
+        }
+
+        if event == Event.START_TREE:
+            self.room_state[self.active_room] = RoomState.PRIMARY
+        else:
+            self.room_state[self.active_room] = RoomState.HIGH
+        # FIXME: play music
